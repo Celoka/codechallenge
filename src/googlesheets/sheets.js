b3c4ca5c1a534cfa505a466ff34d34d0b024c95f
Celoka <eloka.chima@gmail.com>
2020-06-10 23:42:34 +0100

    Update sheets.js

--- src/googlesheets/sheets.js
+++ src/googlesheets/sheets.js
@@ -68,10 +68,13 @@ async function listMajors(auth) {
     ranges: ['IPHONES!A3:J66', 'IPHONES!L3:U66'],
   }, (err, res) => {
     if (err) return console.log(`The API returned an error: ${err}`);
-    const buyRequest = transform(res.data.valueRanges[0].values);
-    const sellRequest = transform(res.data.valueRanges[1].values);
-      writeNewFile('src/buyRequest.json', JSON.stringify(buyRequest));
-      writeNewFile('src/sellRequest.json', JSON.stringify(sellRequest));
+    const dataObj = {
+      'src/buyRequest.json': transform(res.data.valueRanges[0].values),
+      'src/sellRequest.json': transform(res.data.valueRanges[1].values)
+    }
+    for (let [key, value] of Object.entries(dataObj) {
+      writeNewFile(key, JSON.stringify(value));
+    }
   });
 }
 

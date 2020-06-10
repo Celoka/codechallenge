2cf7601c4e134841677b81b960487dbf391163d8
Celoka <eloka.chima@gmail.com>
2020-06-10 23:33:12 +0100

    Delete routes.js

--- src/database/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createRequestData, getRequestData } from '../../controllers/controllers';
-
-const router = express.Router();
-
-router.get('/data', getRequestData);
-router.post('/data', createRequestData);
-
-export default router;
